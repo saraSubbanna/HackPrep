38c0536efbbb529c8a00fcbf2d8382858702fc20
saraSubbanna <sarasubbanna@gmail.com>
2016-04-16 17:42:33 -0700

    First commit

---
+++ HackPrep/HackLeftTableViewCell.swift
@@ -0,0 +1,88 @@
+//
+//  TableViewCell.swift
+//  CodeCollab
+//
+//  Created by Sara Subbanna on 4/16/16.
+//  Copyright (c) 2016 Avinash Jain. All rights reserved.
+//
+
+import UIKit
+
+class HackLeftTableViewCell: UITableViewCell {
+    
+    
+    var redBall: UIImage = (UIImage(named: "Red Ball") as UIImage?)!
+    var blueBall: UIImage = (UIImage(named: "Blue Ball") as UIImage?)!
+    var greenBall: UIImage = (UIImage(named: "Green Ball") as UIImage?)!
+    var purpleBall: UIImage = (UIImage(named: "Purple Ball") as UIImage?)!
+    var orangeBall: UIImage = (UIImage(named: "Orange Ball") as UIImage?)!
+    
+    var title: String!
+    var date: String!
+    var address: String!
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellName: UILabel!
+    @IBOutlet weak var cellAddress: UILabel!
+    @IBOutlet weak var cellDate: UILabel!
+    
+    @IBOutlet weak var rightCellAddress: UILabel!
+    @IBOutlet weak var rightCellDate: UILabel!
+    @IBOutlet weak var rightCellName: UILabel!
+    @IBOutlet weak var rightCellImage: UIImageView!
+    
+    func loadItems (name: String, address: String, date: String, name2: String, address2: String, date2: String) {
+        
+        var setter = Int(arc4random_uniform(5))
+        if setter == 0 {
+            cellImage.image = blueBall
+        }
+        if setter == 1 {
+            cellImage.image = redBall
+        }
+        if setter == 2 {
+            cellImage.image = greenBall
+        }
+        if setter == 3 {
+            cellImage.image = orangeBall
+        }
+        if setter == 4 {
+            cellImage.image = purpleBall
+        }
+        setter = Int(arc4random_uniform(5))
+        if setter == 0 {
+            rightCellImage.image = blueBall
+        }
+        if setter == 1 {
+            rightCellImage.image = redBall
+        }
+        if setter == 2 {
+            rightCellImage.image = greenBall
+        }
+        if setter == 3 {
+            rightCellImage.image = orangeBall
+        }
+        if setter == 4 {
+            rightCellImage.image = purpleBall
+        }
+        
+        cellName.text = name
+        cellAddress.text = address
+        cellDate.text = date
+        
+        
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HackPrep/HackathonViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Sara Subbanna on 4/16/16.
+//  Copyright (c) 2016 Srividhya Gopalan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class HackathonViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var hackathons: [Hackathon] = []
+    
+    @IBOutlet weak var leftTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        leftTable.delegate = self
+        leftTable.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+        getHackathons()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getHackathons() {
+        for i in
+            hackathons.append(Hackathon(name: "Test 1", image: (UIImage(named: "Purple Ball") as UIImage?)!, date: NSDate(), blurb: "blurb", location: CLLocationCoordinate2D(), teamSize: 4, prizes: [], members: [], address: "My house"))
+        hackathons.append(Hackathon(name: "Test 1", image: (UIImage(named: "Purple Ball") as UIImage?)!, date: NSDate(), blurb: "blurb", location: CLLocationCoordinate2D(), teamSize: 4, prizes: [], members: [], address: "My house"))
+        hackathons.append(Hackathon(name: "Test 1", image: (UIImage(named: "Purple Ball") as UIImage?)!, date: NSDate(), blurb: "blurb", location: CLLocationCoordinate2D(), teamSize: 4, prizes: [], members: [], address: "My house"))
+        hackathons.append(Hackathon(name: "Test 1", image: (UIImage(named: "Purple Ball") as UIImage?)!, date: NSDate(), blurb: "blurb", location: CLLocationCoordinate2D(), teamSize: 4, prizes: [], members: [], address: "My house"))
+        hackathons.append(Hackathon(name: "Test 1", image: (UIImage(named: "Purple Ball") as UIImage?)!, date: NSDate(), blurb: "blurb", location: CLLocationCoordinate2D(), teamSize: 4, prizes: [], members: [], address: "My house"))
+        hackathons.append(Hackathon(name: "Test 1", image: (UIImage(named: "Purple Ball") as UIImage?)!, date: NSDate(), blurb: "blurb", location: CLLocationCoordinate2D(), teamSize: 4, prizes: [], members: [], address: "My house"))
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 3
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //self.selectedCell = indexPath.row
+        //self.performSegueWithIdentifier("To Deep Look", sender: self)
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Left Cells", forIndexPath: indexPath) as! HackLeftTableViewCell
+        var row = indexPath.row
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd MMMM yyyy"
+        var dateString = dateFormatter.stringFromDate(hackathons[row].date)
+        var dateString2 = dateFormatter.stringFromDate(hackathons[row+1].date)
+        cell.loadItems(hackathons[row].name, address: hackathons[row].address, date: dateString, name2: hackathons[row+1].name, address2: hackathons[row+1].address, date2: dateString2)
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    
+    
+}
+
